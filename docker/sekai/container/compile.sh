c932f032aac9bafca5f815374a501e468274ff89
Alvaro Cano <alvaro.cano@zevenet.com>
2021-04-23 18:05:57 +0200

    create a simple docker to build and run sekaid
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

---
+++ docker/sekai/container/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+exec 2>&1
+set -e
+set -x
+
+GOLANG_ARCH="amd64"
+GO_VERSION="1.15.6"
+GO_TAR=go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz
+
+echo "INFO: Installing latest go $GOLANG_ARCH version $GO_VERSION https://golang.org/doc/install ..."
+cd /tmp
+
+curl -s	https://dl.google.com/go/$GO_TAR -o $GO_TAR
+tar -C /usr/local -xvf $GO_TAR &>/dev/null
+
+git clone https://github.com/KiraCore/sekai
+cd sekai
+
+/usr/local/go/bin/go build ./cmd/sekaid
+mv sekaid /usr/bin/
+ln -s /usr/bin/sekaid /root
+
+# clean
+rm -rf /go /usr/bin/go
